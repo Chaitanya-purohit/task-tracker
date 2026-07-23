8cf83fa775b928be3f74fb37976cd6abc066f918
Chaitanya Purohit <purohitchaitanya306@gmail.com>
2026-07-24 02:41:54 +0530

    Add list command

--- taskTracker.py
+++ taskTracker.py
@@ -17,7 +17,7 @@ def save_tasks(tasks):
     with open(FILE, "w") as f:
         json.dump(tasks, f, indent = 2)
 
-if len(argv) < 2:
+if len(sys.argv) < 2:
     print("Usage: python taskTracker.py <command> [args]")
     sys.exit(1)
 
@@ -28,7 +28,7 @@ if command == "add":
     tasks = load_tasks()
     now = datetime.now().isoformat()
 
-    new_id = max((t["id" for t in tasks]), default = 0) + 1
+    new_id = max((t["id"] for t in tasks), default = 0) + 1
 
     task = {
         "id" : new_id,
@@ -40,3 +40,20 @@ if command == "add":
     tasks.append(task)
     save_tasks(tasks)
     print(f"Task added successfully (ID: {new_id})")
+elif command == "list":
+    tasks = load_tasks()
+    l = []
+    if (len(sys.argv) > 2):
+        status = sys.argv[2]
+        for t in tasks:
+            if t["status"] == status:
+                l.append(t)
+    elif len(sys.argv) == 2:
+        l = tasks
+    if len(l) == 0:
+        print("No tasks found")
+    else: 
+        for ta in l:
+            print(f"[{ta['id']}] {ta['description']} - {ta['status']}")
+
+        
